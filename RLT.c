9ca9034453c9fd5a70a4cbd10c6be9075bcba814
Russ <russellclarke82@gmail.com>
2020-09-05 23:36:22 +0100

    Merger

---
+++ RLT.c
@@ -0,0 +1,78 @@
+/**
+ * Learn from the probability of time and question the probability.
+ * An expansion from merging Thyme project with QBits and Particulates
+ * @Created and finished between 04.09.2020 and 05.09.2020
+ * @Listening Peace Orchestra.
+ * @By Russell A E Clarke Et. al
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+#include <time.h>
+
+float A;
+float T;
+float C;
+float PT;
+float FT;
+float V;
+float PI;
+float DIM;
+float SIG(C);
+float VEL;
+float IN;
+float RAD = 1;
+
+float WEIGHT;
+float weights;
+float EPSILON;
+float SIGMOID;
+float SIGMOIDPRIME;
+float COST;
+float BIAS;
+float REWARD;
+float PROBABILITY;
+float MDP;
+float Q;
+float R;
+float INPUT;
+float HIDDEN;
+float OUTPUT;
+
+float main () {
+
+    #define IN T;
+    #define SIG ((1) / (-1(C--)));
+    #define PI 3.1415926535;
+    #define A (pow((PI * RAD), DIM));
+    #define V (time1 - time2);
+    #define VEL [T, V];
+    #define PT ((C % FT) / T); 
+    #define C exp(pow(A), ((exp(--IN)) / (exp(IN++))));
+    #define FT (((T) / C) - PT); 
+    #define T exp(((PT + FT) / VEL) / (pow(exp(VEL))), (exp(IN, DIM))));
+
+    #define WEIGHT modf(((randn() % 1.0) + 0.0), float *wdiscard);
+    #define EPSILON 5E-5;
+    #define SIGMOID (1.0 / (-1 + (exp()--))));
+    #define SIGMOIDPRIME (1 * (-1 + (exp()--))));
+    #define GRAD_DESCENT pow(((COST) - (COST(EPSILON))), 2);
+    #define COST (weights, BIAS);
+    #define BIAS modf(((randn() % 1.0) + 0.0), float *bdiscard);
+    #define N INPUT++;
+    #define OUT SIGMOID((weights * N) + BIAS);
+    #define Y (0 ?: 1);
+    #define REWARD (Y / (input, N));
+    #define PROBABILITY (N / (input, Y));
+    #define MDP (PROBABILITY / (REWARD + BIAS));
+    #define Q pow(pow(MDP, N), 2) + Y;
+    #define R (Q / (REWARD + BIAS));
+
+    #define INPUT (IN, OUT, VEL) * WEIGHT + BIAS;
+    #define HIDDEN (10 * pow(20, 4)) * WEIGHT;
+    #define OUTPUT (5 * WEIGHT);
+    
+    return 1;
+}
