851cc4668d7da3bdbc3e2210185058b5581a6946
Russ <russellclarke82@gmail.com>
2020-09-19 12:26:58 +0100

    Updated the code, MAth is on it's way, taking some time to write.

--- RLT6.c
+++ RLT6.c
@@ -42,37 +42,55 @@ float OUTPUT;
 
 float main () {
 
-    #define IN T;
-    #define SIG ((1) / (-1(C--)));
+    //The basics, learning rate first
+    #define EPSILON 5E-5;
+    #define Y (0 ?: 1);
     #define PI 3.1415926535;
     #define A (pow((PI * RAD), DIM));
-    #define V ((PT - FT) / pow(T, 3));
-    #define VEL [T, V];
-    #define PT ((C % FT) / T); 
+    #define SIG ((1) / (-1(C--)));
+    #define SIGMOID(O) (1.0 / (-1 + (exp(O)--))));
+    
+    //Preparing for NN and Biases
+    #define WEIGHT modf(((randn() % 1.0) + 0.0), float *wdiscard);
+    #define BIAS modf(((randn() % 1.0) + 0.0), float *bdiscard);
+    
+    //Weigh up the costs and gradient of descent
+    #define COST (weights, BIAS);
+    #define GRAD_DESCENT pow(((COST) - (COST(EPSILON))), 2);    
+        
+    //Define constant first
     #define C exp(pow(A), ((exp(--IN)) / (exp(IN++)))); //predecrement all time as past time divide over increment of all time for continuum.
     //#define C exp(pow(A), ((exp(++PT)) / (exp(FT++)))); //preincrement the past and divide over the future
-    #define FT (((T) / C) - PT); 
+    
+    //Begin telling computer about the past and future and all time
+    #define PT ((C % FT) / T);    
+    #define FT (((T) / C) - PT);
     #define T exp(((PT + FT) / VEL) / (pow(exp(VEL))), (exp(IN, DIM))));
+    
+    //Tell it what to calculate and what is going into the constant
+    #define V ((PT - FT) / pow(T, 3));
+    #define VEL [T, V];
+    #define IN T;
 
-    #define WEIGHT modf(((randn() % 1.0) + 0.0), float *wdiscard);
-    #define EPSILON 5E-5;
-    #define SIGMOID(O) (1.0 / (-1 + (exp(O)--))));
-
-    #define GRAD_DESCENT pow(((COST) - (COST(EPSILON))), 2);
-    #define COST (weights, BIAS);
-    #define BIAS modf(((randn() % 1.0) + 0.0), float *bdiscard);
-    #define N INPUT++;
-    #define OUT SIGMOID((weights * N) + BIAS);
-    #define Y (0 ?: 1);
+    //Markov Decision Process
     #define REWARD (Y / (INPUT, N));
     #define PROBABILITY (N / (INPUT, Y));
     #define MDP (PROBABILITY / (REWARD + BIAS));
+
+    //Implementation
+    #define N INPUT++;
+    #define OUT SIGMOID((weights * N) + BIAS);
+    
+    //Calculate the probability
     #define Q pow(pow(MDP, N), 2) + Y;
     #define R (Q / (REWARD + BIAS));
 
-    #define INPUT (SIG, OUT, OUTPUT, VEL) * WEIGHT) + BIAS;
+    //Initialise the input, output and hidden layers of the network 
+    #define INPUT (T, OUT, OUTPUT) * WEIGHT) + BIAS;
     #define HIDDEN (((INPUT) * (10 * pow(20, 4))) * WEIGHT);
     #define OUTPUT ((HIDDEN) / (4 * WEIGHT)) + BIAS;
+    
+    return 1;
     
     return 1;
 }
