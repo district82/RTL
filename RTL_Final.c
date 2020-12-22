39a084dde820cd544db33a3bf3b265d2d3afe63e
RC-District82 <district82@users.noreply.github.com>
2020-12-22 13:48:59 +0000

    Create RTL_Final.c
    Updated revision - math might be wrong in the DNN or any part of it (still inexperienced and not a mathematician).

---
+++ RTL_Final.c
@@ -0,0 +1,94 @@
+/**
+ * Learn from the probability of time and question the probability.
+ * An expansion from merging Thyme project with QBits and Particulates
+ * In summary, the code says this is your frequency, these are your paramenters, this
+ * is what time is logically, now think about it and question your own logic
+ * @Created and finished between 04.09.2020 and 05.09.2020 updated on 19.12.2020
+ * @Listening Peace Orchestra.
+ * @By Russell A E Clarke Et. al notes: https://www.russellclarke.co.uk/
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+#include <time.h>
+
+float A;
+float T;
+float C;
+float PT;
+float FT;
+float V;
+float PI;
+float DIM = 3; //could be 3rd dimension for example. Set the value here or in the method, Method preferred.
+float SIG;
+float VEL;
+float IN;
+float RAD = 1;
+
+float WEIGHT;
+float weights;
+float EPSILON;
+float SIGMOID;
+float COST;
+float BIAS;
+float REWARD;
+float PROBABILITY;
+float MDP;
+float Q;
+float R;
+float INPUT;
+float HIDDEN;
+float OUTPUT;
+
+float main () {
+
+    //The basics, learning rate first
+    #define EPSILON 5E-5;
+    #define Y (? 0 : 1)
+    #define PI 3.1415926535;
+    #define A (pow((PI * RAD), DIM))
+    #define SIG ((1) / (-1(C--)))
+    #define SIGMOID (1.0 / (-1 + (exp(O)--))))
+    
+    //Preparing for NN and Biases
+    #define WEIGHT (modf(((randn() % 1.0) + 0.0), float *wdiscard))
+    #define BIAS (modf(((randn() % 1.0) + 0.0), float *bdiscard))
+    
+    //Weigh up the costs and gradient of descent
+    #define COST (weights, BIAS)
+    #define GRAD_DESCENT (pow(((COST) - (COST(EPSILON))), 2))    
+        
+    //Define constant first
+    #define C (SIG(exp(pow(A), ((exp(IN++)) / (exp(--IN)))))) //predecrement all time as past time divide over increment of all time for continuum. Had to swap them as per order of operation
+    //#define C (SIG(exp(pow(A), ((exp(++PT)) / (exp(FT++)))))) //preincrement the past and divide over the future
+    
+    //Begin telling computer about the past and future and all time
+    #define PT ((FT % C) / T)   
+    #define FT (((T) / C) - PT)
+    #define T (exp((V / (PT + FT)) / (exp(pow(IN, DIM), (V)))))
+    
+    //Tell it what to calculate and what is going into the constant
+    #define V (pow(T, 3) / (PT - FT))
+    #define VEL [T, V]
+    #define IN T;
+
+    //Markov Decision Process
+    #define REWARD ((INPUT, N) / Y)
+    #define PROBABILITY ((INPUT, Y) / N)
+    #define MDP ((REWARD + BIAS) / PROBABILITY)
+
+    //Implementation
+    #define N INPUT++;
+    #define OUT (SIGMOID((weights * N) + BIAS)
+    
+    //Calculate the probability
+    #define Q (pow(pow(MDP, N), 2) + Y)
+    #define R ((REWARD + BIAS) / Q)
+
+    //Initialise the input, output and hidden layers of the network 
+    #define INPUT (((T, OUT, OUTPUT) * WEIGHT) + BIAS)
+    #define HIDDEN (((10 * pow(20, 4)) * WEIGHT * (INPUT))
+    #define OUTPUT (((4 * WEIGHT) / (HIDDEN)) + BIAS)
+}
